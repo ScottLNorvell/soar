ad31a4e3b8cbe44d312d0beade9f290490da79b6
Scott Norvell <scottlnorvell@gmail.com>
2016-03-02 16:50:33 -0500

    More commits

--- app/utils/article-fixture.js
+++ app/utils/article-fixture.js
@@ -7,6 +7,10 @@ export default {
 
   Brett Phillips is a highly regarded outfield prospect for the Milwaukee Brewers. ESPN's Keith Law has him ranked as the 46th best prospect in all of baseball. Now, I don't spend as much time as Keith does scouting minor leaguers, but I'm guessing that Keith based his rankings on things like fielding ability, bat speed, approach at the plate, baseball IQ, etc. Those are all fine and good I suppose if your goal is to just find the best baseball players. But what about intangibles? I'm not talking about David Eckstein-like grit and other fake things old sportswriters think about instead of using Viagra. I'm talking about making sure your player isn't so annoying that he distracts the rest of the clubhouse from being able to focus on the game. It's with that in mind, that I give you the darker side of Brewers' prospect Brett Phillips. His demon laugh:
 
+[#image: /photos/56d5ddasd345]|||
+OMG, make it stop!
+|||
+
 [#iframe: https://www.youtube.com/watch?v=kpG-fUv9vZs]||||||
 
 Now, I'm sure some of you are thinking to yourself, "Jack, what are you talking about? That donkey laugh is hilarious." And sure, it's hilarious when you hear it once in a video that you voluntarily clicked on. But what about when you've just finished game 140 of 162 and you're stressed and things aren't going great and suddenly you hear that goddamn demonic donkey laugh for the millionth time? You'll lose your mind and probably throw a Gatorade cooler at him. That's bad for clubhouse morale. So, Brett, you seem goofy and charming, and that's great, but get your shit together and learn how to laugh in a way that won't make your teammates hate you in the dog days of summer.`,
