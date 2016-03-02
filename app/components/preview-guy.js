b8e276da50344c6dcbbd9d865af226bcbe7c34ff
kvignos <kathleen_vignos@wired.com>
2016-03-02 16:50:14 -0500

    add the highlighting to the headline

--- app/components/preview-guy.js
+++ app/components/preview-guy.js
@@ -4,15 +4,20 @@ export default Ember.Component.extend({
   classNames: [ 'md-preview show' ],
   text: Ember.computed('model', function()
   		{
+  			var previewBodyParagraphs = this.get('model');
+  			// TODO: replace with real keywords
   			var keyword1 = 'Brett Phillips';
   			var keyword2 = 'Milwaukee Brewers';
-  			var previewBodyParagraph = this.get('model');
-  			var highlightBodyParagraph = previewBodyParagraph
-  			.replace(new RegExp(keyword1, 'g'), '<span class="keyword-highlight">' + keyword1 + '</span>')
-  			.replace(new RegExp(keyword2, 'g'), '<span class="keyword-highlight">' + keyword2 + '</span>')
-			.replace(new RegExp('\n\n', 'g'), '</p>');
-  			highlightBodyParagraph = '<p>' + highlightBodyParagraph + '</p>';
-  			return highlightBodyParagraph;
+  			var firstParagraph = 'This noise might haunt you forever.';
+  			var hed = 'Baseball Player\'s Laugh Sounds Like a Cackling Demon';
+
+  	 		var highlightedParagraphs = previewBodyParagraphs
+  	 		.replace(new RegExp(keyword1, 'g'), '<span class="keyword-highlight">' + keyword1 + '</span>')
+  	 		.replace(new RegExp(keyword2, 'g'), '<span class="keyword-highlight">' + keyword2 + '</span>')
+  	 		.replace(new RegExp(firstParagraph, 'g'), '<span class="keyword-highlight">' + firstParagraph + '</span>')
+			.replace(new RegExp(hed, 'g'), '<span class="keyword-highlight">' + hed + '</span>')
+  	 		;
+  	 		return highlightedParagraphs.split('\n\n');
   		}
 	)
  });
